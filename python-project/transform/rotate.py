76bb96b7b66c1ff0e574befd70887f1098053f94
Maxime Chéramy <maxime@cheramy.org>
2017-08-08 23:41:52 +0200

    add rotation

--- python-project/transform/rotate.py
+++ python-project/transform/rotate.py
@@ -1,4 +1,6 @@
 from PIL import Image, ImageDraw
+from math import sin, cos, pi
+
 
 # Load image:
 input_image = Image.open("input.png")
@@ -8,10 +10,17 @@ input_pixels = input_image.load()
 output_image = Image.new("RGB", input_image.size)
 draw = ImageDraw.Draw(output_image)
 
+angle = pi / 3  # angle in radian
+center_x = input_image.width / 2
+center_y = input_image.height / 2
+
 # Copy pixels
-for x in range(output_image.width):
-    for y in range(output_image.height):
-        xp = input_image.width - x - 1
-        draw.point((x, y), input_pixels[xp, y])
+for x in range(input_image.width):
+    for y in range(input_image.height):
+        # Compute coordinate in input image
+        xp = int((x - center_x) * cos(angle) - (y - center_y) * sin(angle) + center_x)
+        yp = int((x - center_x) * sin(angle) + (y - center_y) * cos(angle) + center_y)
+        if 0 <= xp < input_image.width and 0 <= yp < input_image.height:
+            draw.point((x, y), input_pixels[xp, yp])
 
 output_image.save("output.png")
